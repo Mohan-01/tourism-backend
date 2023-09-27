cb0ddf23f3ac8873f28fd333f914d15110884150
Mohan <mohanvenkata123@gmail.com>
2023-09-28 01:15:54 +0530

    changed to res

--- utils/appError.js
+++ utils/appError.js
@@ -1,15 +1,25 @@
+// class AppError extends Error {
+//   constructor(message, status) {
+//     super(message);
+
+//     this.status = status;
+//     this.statusMessage = `${status}`.startsWith('4') ? 'fail' : 'success';
+//     this.isOperational = true;
+
+//     // Error.toString(this, this.constructor)
+//     console.log(Error.captureStackTrace(this, this.constructor));
+//   }
+// }
+
 class AppError extends Error {
-    constructor(message, status) {
-      super(message);
-  
-      this.status = status;
-      this.statusMessage = `${status}`.startsWith('4') ? 'fail' : 'success';
-      this.isOperational = true;
-  
-      // Error.toString(this, this.constructor)
-      console.log(Error.captureStackTrace(this, this.constructor));
-    }
+  constructor(message, status, res) {
+    super(message);
+    res.status(status).json({
+      statusbar: 'error',
+      message
+    })
   }
-  
+}
+
 export default AppError;
   
